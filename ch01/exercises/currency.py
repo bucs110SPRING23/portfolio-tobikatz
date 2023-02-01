2c9ba867c9a80911b2506ba4f0f730b609a35c6e
Tobi Katz <tkatz4@binghamton.edu>
2023-02-01 18:01:20 -0500

    currency

---
+++ ch01/exercises/currency.py
@@ -0,0 +1,7 @@
+
+rate = input("What is the exchange rate from Euro to Dollar:")
+amount = input("How much do you want to exchange?")
+rate = float(rate)
+amount = float(amount)
+total = rate * amount
+result = print(total-3)
