a6908c7e1e56f8302485f941886ef005cdeec5cf
Tobi Katz <tkatz4@binghamton.edu>
2023-02-13 17:54:11 -0500

    snowman!

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -1,12 +1,14 @@
 import pygame
 pygame.init()
 screen = pygame.display.set_mode()
-pygame.draw.circle(screen, "yellow", [100,100], 100)
+screen.fill("pink")
+pygame.draw.circle(screen, "white", [600,550], 100)
+pygame.draw.circle(screen, "black", [600,550], 100,3)
 pygame.display.flip()
-pygame.draw.circle(screen,"red",[75,75],80)
+pygame.draw.circle(screen,"white",[600,450],75)
+pygame.draw.circle(screen,"black",[600,450],75,3)
 pygame.display.flip()
-pygame.draw.circle(screen,"green",[60,60],60)
+pygame.draw.circle(screen,"white",[600,350],60)
+pygame.draw.circle(screen,"black",[600,350],60,3)
 pygame.display.flip()
 pygame.time.wait(3000)
-
-
