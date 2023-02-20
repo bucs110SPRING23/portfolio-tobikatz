3887a3a43eb785e8d03629ed4106e3d1d9e3f831
Tobi Katz <tkatz4@binghamton.edu>
2023-02-20 17:40:11 -0500

    guess

--- ch03/exercises/guess.py
+++ ch03/exercises/guess.py
@@ -1,3 +1,13 @@
 
 import random
-x = random.randrange(11)
+x = random.randrange(0,11)
+for guesses in range(3):
+    guess =  int(input("Whats the number?"))
+    if guess < x:
+        print("Too Low")
+    elif guess > x:
+        print("Too High")
+    else:
+        print("Correct!")
+        exit()
+        
