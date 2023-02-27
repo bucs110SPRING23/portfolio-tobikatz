8be3dd79c9045659f036ecfebeaaf7acdc2da3e2
Tobi Katz <tkatz4@binghamton.edu>
2023-02-27 17:28:10 -0500

    coin

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,7 +1,8 @@
 import turtle
 window = turtle.Screen()
 player1 = turtle.Turtle()
-x = len(window)[0]/2
-y = len(window)[1]/2
+size = turtle.screensize()
+x = size[0]/2
+y = size[1]/2
 player1.goto(x,y)
 window.wait(5000)
