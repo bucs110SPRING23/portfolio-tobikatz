7cb6df2e4046f445ab4db5ae461f8f86b28aee22
Tobi Katz <tkatz4@binghamton.edu>
2023-03-06 17:34:00 -0500

    coinflip!

---
+++ ch05/exercises/notes.py
@@ -0,0 +1,32 @@
+# # vending machines
+# print("Welcome to the vending machine")
+# code = input("Please enter a code: ")
+# money = int(input("Give me money: "))
+
+# def myvendingmachine():
+#     if code == "A":
+#         if money >= 1:
+#             print("You got a coke")
+#         else:
+#             print("You need more money")
+#     else:
+#         print("invalid code")
+
+# myvendingmachine()
+
+##determine maximum value
+def findmax():
+    max = a
+    if b > max:
+        max = b
+    if c > max:
+        max = c
+    print(max)
+
+print("Please enter 3 numbers: ")
+a = int(input(": "))
+b = int(input(": "))
+c = int(input(": "))    
+
+findmax()
+    
