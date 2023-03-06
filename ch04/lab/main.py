ca855f498758b5d0e37ec0c56ddb707074b68a0b
Tobi Katz <tkatz4@binghamton.edu>
2023-03-06 11:20:00 -0500

    lab 4!!

--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -24,7 +24,7 @@ pygame.display.flip()
 playergreen = []
 playerpink = []
 
-results= ()
+results= ""
 
 done = False
 hitboxes = {
@@ -39,81 +39,76 @@ colors = {
     "green" : (34,139,34)
 }
 
-while not done:
-    for c,hb in hitboxes.items():
-            pygame.draw.rect(screen,colors[c],hb)
+for c,hb in hitboxes.items():
+        pygame.draw.rect(screen,colors[c],hb)
+pygame.display.flip()
+
+while results == "":
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             if hitboxes["pink"].collidepoint(event.pos):
                 results = "pink"
             elif hitboxes["green"].collidepoint(event.pos):
                 results = "green" 
-    for _ in range(11):
-                for i in range(2):
-                    dartx = random.randrange(screensize [0])
-                    darty = random.randrange(screensize [1])
-                    distance_from_center = math.hypot(x-dartx,y-darty)
-                    is_in_circle = distance_from_center <= x
-                    if i == 0:
-                        if is_in_circle: 
-                            dart = pygame.draw.circle(screen, "springgreen4", [dartx,darty], 15)
-                            pygame.display.flip()
-                            pygame.time.wait(500)
-                            playergreen.append("hit")
-                        elif not is_in_circle: 
-                            dart = pygame.draw.circle(screen, "green", [dartx,darty], 15)
-                            pygame.display.flip()
-                            pygame.time.wait(500)
-                            
-                    elif i == 1 :
-                        if is_in_circle: 
-                            dart = pygame.draw.circle(screen, "pink", [dartx,darty], 15)
-                            pygame.display.flip()
-                            pygame.time.wait(500)
-                            playerpink.append("hit")
-                        elif not is_in_circle:
-                            dart = pygame.draw.circle(screen, "pink4", [dartx,darty], 15)
-                            pygame.display.flip()
-                            pygame.time.wait(500)
-           
-            
-                            
 
-    print(playerpink)
-    print(playergreen)
+for _ in range(11):
+    for i in range(2):
+        dartx = random.randrange(screensize [0])
+        darty = random.randrange(screensize [1])
+        distance_from_center = math.hypot(x-dartx,y-darty)
+        is_in_circle = distance_from_center <= x
+        if i == 0:
+            if is_in_circle: 
+                dart = pygame.draw.circle(screen, "springgreen4", [dartx,darty], 15)
+                pygame.display.flip()
+                pygame.time.wait(500)
+                playergreen.append("hit")
+            elif not is_in_circle: 
+                dart = pygame.draw.circle(screen, "green", [dartx,darty], 15)
+                pygame.display.flip()
+                pygame.time.wait(500)
+                
+        elif i == 1 :
+            if is_in_circle: 
+                dart = pygame.draw.circle(screen, "pink", [dartx,darty], 15)
+                pygame.display.flip()
+                pygame.time.wait(500)
+                playerpink.append("hit")
+            elif not is_in_circle:
+                dart = pygame.draw.circle(screen, "pink4", [dartx,darty], 15)
+                pygame.display.flip()
+                pygame.time.wait(500)
+        
+print(playerpink)
+print(playergreen)
 
-    if len(playergreen) > len(playerpink):
-        if results == "green":
-            font = pygame.font.Font(None, 48)
-            text = font.render("Green player won, correct!", True, "black")
-            screen.blit(text, (x,y))
-            pygame.display.flip()
-        else:
-            font = pygame.font.Font(None, 48)
-            text = font.render("Green player won, incorrect!", True, "black")
-            screen.blit(text, (x,y))
-            pygame.display.flip()
-
-    elif len(playergreen) < len(playerpink):
-        if results == "pink":
-            font = pygame.font.Font(None, 48)
-            text = font.render("Pink player won, correct!", True, "black")
-            screen.blit(text, (x,y))
-            pygame.display.flip()
-        else:
-            font = pygame.font.Font(None, 48)
-            text = font.render("Pink player won, incorrect!", True, "black")
-            screen.blit(text, (x,y))
-            pygame.display.flip()
-
+font = pygame.font.Font(None, 48)
+if len(playergreen) > len(playerpink):
+    if results == "green":
+        text = font.render("Green player won, correct!", True, "black")
+        screen.blit(text, (x,y))
+        pygame.display.flip()
     else:
-        if results == "pink" or "green":
-            font = pygame.font.Font(None, 48)
-            text = font.render("Tie Game!", True, "black")
-            screen.blit(text, (x,y))
-            pygame.display.flip()
+        font = pygame.font.Font(None, 48)
+        text = font.render("Green player won, incorrect!", True, "black")
+        screen.blit(text, (x,y))
+        pygame.display.flip()
+elif len(playergreen) < len(playerpink):
+    if results == "pink":
+        font = pygame.font.Font(None, 48)
+        text = font.render("Pink player won, correct!", True, "black")
+        screen.blit(text, (x,y))
+        pygame.display.flip()
+    else:
+        font = pygame.font.Font(None, 48)
+        text = font.render("Pink player won, incorrect!", True, "black")
+        screen.blit(text, (x,y))
+        pygame.display.flip()
+else:
+    font = pygame.font.Font(None, 48)
+    text = font.render("Tie Game!", True, "black")
 
-    
-    done = True
+screen.blit(text, (x,y))
+pygame.display.flip()
 
 pygame.time.wait(1000)
