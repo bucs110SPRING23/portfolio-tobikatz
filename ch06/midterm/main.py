f9b880a46b5a527c5febd02ea6fc3a4e13a0db32
Tobi Katz <tkatz4@binghamton.edu>
2023-03-19 18:01:08 -0400

    nograph

---
+++ ch06/midterm/main.py
@@ -0,0 +1,22 @@
+import turtle
+import math 
+
+def screen_dimensions():
+    window = turtle.Screen()
+    window.bgcolor('gold')
+
+def shapes ():
+    pen = turtle.Turtle()
+    pen.color('black')
+    pen.shape('circle')
+
+
+def main():
+    screen_dimensions()
+    shapes()
+
+
+
+
+
+main ()
