1f65e8654dfba19273c90d5cc8e43dbd4a5721c2
Tobi Katz <tkatz4@binghamton.edu>
2023-03-19 21:50:42 -0400

    dealthy hallows!

--- ch06/midterm/main.py
+++ ch06/midterm/main.py
@@ -1,32 +1,45 @@
 import turtle
 import math 
 
-def screen_dimensions():
-    '''
-   general function description: this function creates a screen
-   args: (type) description
-   return: (type) description
-   '''
-    window = turtle.Screen()
-    window.bgcolor('gold')
 
-def shapes ():
+def deathly_hallows (side_length = 120):
     '''
-   general function description: This function draws the shapes
-   args: (type) description
-   return: (type) description
+   general function description: This function creates a screen and draws three shapes to create the harry potter deathly hallows symbol (a triangle, circle, and line)
+   args: this function takes the argument of side_length, which allows for adjustments of the size of the shapes. 
+   return: this function returns the amount of sides of the original shape i.e. the triangle
    '''
+    # this sets up the screen
+    screen = turtle.Screen()
+    turtle.screensize(500,500,"gold")
     pen = turtle.Turtle()
-    pen.color('black')
+    pen.color('red')
     pen.shape('circle')
+
+    # invisibility cloak
+    triangle = 3
+    angle = 360/triangle
+    
+    for i in range(triangle):
+        pen.forward(side_length)
+        pen.left(angle)
+        pen.forward(side_length)
+
+    # sorcerors stone
+
+    wand = math.sqrt(3) * side_length
+    radius = (wand/3)
+    pen.circle(radius)
+
+    # elder wand
+    pen.left(90)
+    pen.forward(wand)
+    
+    screen.exitonclick()
+    return triangle 
 
 
 def main():
-    screen_dimensions()
-    shapes()
-
-
-
+   deathly_hallows(193)
 
 
 main ()
