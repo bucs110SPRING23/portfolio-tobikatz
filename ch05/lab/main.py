4e5923602ce950d4b53d47c276c1756249271e95
Tobi Katz <tkatz4@binghamton.edu>
2023-03-20 11:16:15 -0400

    lab!

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -18,13 +18,13 @@ def threenp1range(upper_limit):
         threenplus1_dictionary[n] = iters
     return threenplus1_dictionary
 
-def graph (threennplus1_dictionary):
-    points = threenp1range(threennplus1_dictionary)
+def graph (value):
+    points = threenp1range(value)
     coords = points.items()
     print(coords)
     pygame.init()
     screen = pygame.display.set_mode((500,500))
-    pygame.draw.lines(screen, "orange", True, list(coords))
+    pygame.draw.lines(screen, "orange", False, list(coords))
     pygame.display.flip()
     new_screen = pygame.transform.flip(screen, False,True)
     width, height = new_screen.get_size()
@@ -51,9 +51,9 @@ def graph (threennplus1_dictionary):
 
 
 def main():
-   value = 6000
+   value = 600
    print (threenp1(value))
    print (threenp1range(value))
-   graph(threenp1(value))
+   graph(value)
 
 main ()
